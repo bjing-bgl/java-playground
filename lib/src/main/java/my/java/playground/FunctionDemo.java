12beef2e5161e6da2cddfdcb406b860296e4fe95
bjing-bgl <bjing@bglcorp.com.au>
2026-06-29 16:38:26 +1000

    Add project template

---
+++ lib/src/main/java/my/java/playground/FunctionDemo.java
@@ -0,0 +1,7 @@
+package my.java.playground;
+
+public class FunctionDemo {
+  // TODO compose simple functions
+
+  // TODO compose more complex functions
+}
---
+++ lib/src/test/java/my/java/playground/FunctionDemoTest.java
@@ -0,0 +1,4 @@
+package my.java.playground;
+
+public class FunctionDemoTest {
+}
